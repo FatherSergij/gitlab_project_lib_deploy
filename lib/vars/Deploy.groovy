357d4d63930a12d4d2430d37bbcab360de007112
root user <roo@localhost>
2022-04-05 04:53:33 +0000

    d

--- lib/vars/Deploy.groovy
+++ lib/vars/Deploy.groovy
@@ -22,22 +22,24 @@ def call(String branch, String tag, String service) {
   //     envsubst < deploy-${service}.yaml | kubectl apply -f -;"""')
   // } else {
     //echo "${tag}"
-      this.script.sh('ssh ubuntu@${IP_K8S} \
-      """cd repos/project_lib_deploy/yaml; \
-      export BRANCH=${branch}; \
-      export TAG=${tag}; \
-      export SERVICE=${service}; \
-      kubectl create namespace ${branch}; \
-      kubectl apply -f issuer.yaml; \
-      envsubst < ingress.yaml | kubectl apply -f -; \
-      kubectl delete -n ${branch} secret regcred --ignore-not-found; \
-      kubectl create secret docker-registry regcred \
-              --docker-server=${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com \
-              --docker-username=AWS \
-              --docker-password=$(aws ecr get-login-password --region ${AWS_REGION}) \
-              --namespace=${branch}; \
-      envsubst < service-${service}.yaml | kubectl apply -f -; \
-      kubectl delete deploy deploy-${service} -n ${branch}; \
-      envsubst < deploy-${service}.yaml | kubectl apply -f -;"""')
+      this.script.sh("aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS \
+      --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com")
+      //   'ssh ubuntu@${IP_K8S} \
+      // """cd repos/project_lib_deploy/yaml; \
+      // export BRANCH=${branch}; \
+      // export TAG=${tag}; \
+      // export SERVICE=${service}; \
+      // kubectl create namespace ${branch}; \
+      // kubectl apply -f issuer.yaml; \
+      // envsubst < ingress.yaml | kubectl apply -f -; \
+      // kubectl delete -n ${branch} secret regcred --ignore-not-found; \
+      // kubectl create secret docker-registry regcred \
+      //         --docker-server=${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com \
+      //         --docker-username=AWS \
+      //         --docker-password=$(aws ecr get-login-password --region ${AWS_REGION}) \
+      //         --namespace=${branch}; \
+      // envsubst < service-${service}.yaml | kubectl apply -f -; \
+      // kubectl delete deploy deploy-${service} -n ${branch}; \
+      // envsubst < deploy-${service}.yaml | kubectl apply -f -;"""')
    // }
 }
