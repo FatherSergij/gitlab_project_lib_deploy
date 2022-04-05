20678b6785c30af25700558d86a766c6116a0194
root user <roo@localhost>
2022-04-05 07:48:27 +0000

    d

--- lib/vars/Deploy.groovy
+++ lib/vars/Deploy.groovy
@@ -21,8 +21,9 @@ def call(String branch_dep, String tag_dep, String service_dep) {
   //     kubectl delete deploy deploy-${service} -n ${branch}; \
   //     envsubst < deploy-${service}.yaml | kubectl apply -f -;"""')
   // } else {
-      sh('ssh ubuntu@${IP_K8S} \
-      """cd repos/project_lib_deploy/yaml; \
+      def ant = new AntBuilder(host: ${IP_K8S}, user: ubuntu, command: 
+      //sh('ssh ubuntu@${IP_K8S} \
+      'cd repos/project_lib_deploy/yaml; \
       export BRANCH="""${branch_dep}"""; \
       export TAG=${tag_dep}; \
       export SERVICE=${service_dep}; \
