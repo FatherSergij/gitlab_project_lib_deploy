cbe1d43afbbaa9768f8ed9a4a75e1a5779979fe3
root user <roo@localhost>
2022-04-05 03:33:56 +0000

    f

--- lib/src/Constants.groovy
+++ lib/src/Constants.groovy
@@ -2,10 +2,4 @@ class Constants {
     static final String IP_K8S="16.170.42.2"
     static final String AWS_ACCOUNT_ID="728490037630"
     static final String AWS_REGION="eu-north-1"    
-    static final String BRANCH="${params.BranchRun}"
-    static final String TAG="${params.ImageTag}" 
-    static final String SERVICE="${params.ServiceRun}"       
-    static final String BRANCH_DEV="${params.BranchRun_dev}"
-    static final String TAG_DEV="${params.ImageTag_dev}" 
-    static final String SERVICE_DEV="${params.ServiceRun_dev}"
 }
--- lib/vars/Deploy.groovy
+++ lib/vars/Deploy.groovy
@@ -1,42 +1,42 @@
 #!/usr/bin/env groovy
 import Constants
 
-def call() {
+def call(String branch, String tag, Staring service) {
   if ("${BRANCH_DEV}" == 'develop') {
       sh 'ssh ubuntu@${IP_K8S} \
       """cd repos/project_lib_deploy/yaml; \
-      export BRANCH=${BRANCH_DEV}; \
-      export TAG=${TAG_DEV}; \
-      export SERVICE=${SERVICE_DEV}; \
-      kubectl create namespace ${BRANCH_DEV}; \
+      export BRANCH=${branch}; \
+      export TAG=${tag}; \
+      export SERVICE=${service}; \
+      kubectl create namespace ${branch}; \
       kubectl apply -f issuer.yaml; \
       envsubst < ingress.yaml | kubectl apply -f -; \
-      kubectl delete -n ${BRANCH_DEV} secret regcred --ignore-not-found; \
+      kubectl delete -n ${branch} secret regcred --ignore-not-found; \
       kubectl create secret docker-registry regcred \
               --docker-server=${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com \
               --docker-username=AWS \
               --docker-password=$(aws ecr get-login-password --region ${AWS_REGION}) \
-              --namespace=${BRANCH_DEV}; \
-      envsubst < service-${SERVICE_DEV}.yaml | kubectl apply -f -; \
-      kubectl delete deploy deploy-${SERVICE_DEV} -n ${BRANCH_DEV}; \
-      envsubst < deploy-${SERVICE_DEV}.yaml | kubectl apply -f -;"""'
+              --namespace=${branch}; \
+      envsubst < service-${service}.yaml | kubectl apply -f -; \
+      kubectl delete deploy deploy-${service} -n ${branch}; \
+      envsubst < deploy-${service}.yaml | kubectl apply -f -;"""'
   } else {
       sh 'ssh ubuntu@${IP_K8S} \
       """cd repos/project_lib_deploy/yaml; \
-      export BRANCH=${BRANCH}; \
-      export TAG=${TAG}; \
-      export SERVICE=${SERVICE}; \
-      kubectl create namespace ${BRANCH}; \
+      export BRANCH=${branch}; \
+      export TAG=${tag}; \
+      export SERVICE=${service}; \
+      kubectl create namespace ${branch}; \
       kubectl apply -f issuer.yaml; \
       envsubst < ingress.yaml | kubectl apply -f -; \
-      kubectl delete -n ${BRANCH} secret regcred --ignore-not-found; \
+      kubectl delete -n ${branch} secret regcred --ignore-not-found; \
       kubectl create secret docker-registry regcred \
               --docker-server=${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com \
               --docker-username=AWS \
               --docker-password=$(aws ecr get-login-password --region ${AWS_REGION}) \
-              --namespace=${BRANCH}; \
-      envsubst < service-${SERVICE}.yaml | kubectl apply -f -; \
-      kubectl delete deploy deploy-${SERVICE} -n ${BRANCH}; \
-      envsubst < deploy-${SERVICE}.yaml | kubectl apply -f -;"""' 
+              --namespace=${branch}; \
+      envsubst < service-${service}.yaml | kubectl apply -f -; \
+      kubectl delete deploy deploy-${service} -n ${branch}; \
+      envsubst < deploy-${service}.yaml | kubectl apply -f -;"""' 
   }
 }
