1ca6970bd0ec71d96c69567421ad201a6131849d
root user <roo@localhost>
2022-04-05 08:27:58 +0000

    ju

--- lib/vars/Deploy.groovy
+++ lib/vars/Deploy.groovy
@@ -23,20 +23,20 @@ def call(String branch_dep, String tag_dep, String service_dep) {
   // } else {
       sh('ssh ubuntu@${IP_K8S} \
       """cd repos/project_lib_deploy/yaml; \
-      export BRANCH="${branch_dep}"; \
-      export TAG="${tag_dep}"; \
-      export SERVICE="${service_dep}"; \
-      kubectl create namespace "${branch_dep}"; \
+      export BRANCH=\"${branch_dep}\"; \
+      export TAG=${tag_dep}; \
+      export SERVICE=${service_dep}; \
+      kubectl create namespace ${branch_dep}; \
       kubectl apply -f issuer.yaml; \
       envsubst < ingress.yaml | kubectl apply -f -; \
-      kubectl delete -n "${branch}" secret regcred --ignore-not-found; \
+      kubectl delete -n ${branch} secret regcred --ignore-not-found; \
       kubectl create secret docker-registry regcred \
-              --docker-server="${Constants.AWS_ACCOUNT_ID}".dkr.ecr."${Constants.AWS_REGION}".amazonaws.com \
+              --docker-server=${Constants.AWS_ACCOUNT_ID}.dkr.ecr.${Constants.AWS_REGION}.amazonaws.com \
               --docker-username=AWS \
-              --docker-password=$(aws ecr get-login-password --region "${Constants.AWS_REGION}") \
-              --namespace="${branch}"; \
-      envsubst < service-"${service}".yaml | kubectl apply -f -; \
-      kubectl delete deploy deploy-"${service}" -n "${branch}"; \
-      envsubst < deploy-"${service}".yaml | kubectl apply -f -;"""')
+              --docker-password=$(aws ecr get-login-password --region ${Constants.AWS_REGION}) \
+              --namespace=${branch}; \
+      envsubst < service-${service}.yaml | kubectl apply -f -; \
+      kubectl delete deploy deploy-${service} -n ${branch}; \
+      envsubst < deploy-${service}.yaml | kubectl apply -f -;"""')
    //}
 }
