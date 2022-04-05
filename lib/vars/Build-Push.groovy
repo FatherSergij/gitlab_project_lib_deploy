b68f43497965f0d702e071dd50206927bd0f683c
root user <roo@localhost>
2022-04-05 14:53:53 +0000

    h

--- lib/vars/Build-Push.groovy
+++ lib/vars/Build-Push.groovy
@@ -2,6 +2,6 @@
 import com.Constants
 
 def call() {
-    sh "docker build src/ -t ${REPOSITORY_URI}:${IMAGE_TAG}"
-    sh "docker push ${REPOSITORY_URI}:${IMAGE_TAG}"
+    sh "docker build src/ -t ${Constants.REPOSITORY_URI}:${Constants.IMAGE_TAG}"
+    sh "docker push ${Constants.REPOSITORY_URI}:${Constants.IMAGE_TAG}"
 }
