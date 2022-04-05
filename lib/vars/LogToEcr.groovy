0c696734aa75b09e6e6fba1a2dfaddd405934ed7
root user <roo@localhost>
2022-04-05 05:06:58 +0000

    g

--- lib/vars/LogToEcr.groovy
+++ lib/vars/LogToEcr.groovy
@@ -3,5 +3,5 @@ import Constants
 
 def call() {
     this.script.sh('aws ecr get-login-password --region """${AWS_REGION}""" | docker login --username AWS \
-      --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com')
+      --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr."""${AWS_REGION}""".amazonaws.com')
 }
