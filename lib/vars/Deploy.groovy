32e06d05a633a75e4c89bdf17dc97fe440c251df
root user <roo@localhost>
2022-04-06 08:37:42 +0000

    f

--- lib/vars/Deploy.groovy
+++ lib/vars/Deploy.groovy
@@ -37,9 +37,9 @@ def call(String branch_dep, String tag_dep, String service_dep) {
         kubectl apply -f issuer.yaml; \
         envsubst < ingress.yaml | kubectl apply -f -; \
         kubectl create secret docker-registry regcred -n ${branch} \
-                --docker-server=${Constants.AWS_ACCOUNT_ID}.dkr.ecr.${Constants.AWS_REGION}.amazonaws.com \
+                --docker-username=AWS \
                 --docker-password=${PASS} \
-                --docker-username=AWS; \
+                --docker-server=${Constants.AWS_ACCOUNT_ID}.dkr.ecr.${Constants.AWS_REGION}.amazonaws.com; \
         envsubst < service-${service}.yaml | kubectl apply -f -; \
         kubectl delete deploy deploy-${service} -n ${branch}; \
         envsubst < deploy-${service}.yaml | kubectl apply -f -;'")
