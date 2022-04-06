1299c6f4b392356d92fc0eb917300c7d633d059b
root user <roo@localhost>
2022-04-06 07:54:28 +0000

    g

--- lib/vars/Build-Push.groovy
+++ lib/vars/Build-Push.groovy
@@ -2,8 +2,7 @@
 import com.Constants
 
 def call(String branch, String tag, String service) {
-    def REPOSITORY_URI="${Constants.AWS_ACCOUNT_ID}.dkr.ecr.${Constants.AWS_REGION}.amazonaws.com/${Constants.IMAGE_REPO_NAME} \
-        _${service}_${branch}"
+    def REPOSITORY_URI="${Constants.AWS_ACCOUNT_ID}.dkr.ecr.${Constants.AWS_REGION}.amazonaws.com/${Constants.IMAGE_REPO_NAME}_${service}_${branch}"
     sh "docker build src/ -t ${Constants.REPOSITORY_URI}:${tag}"
     sh "docker push ${Constants.REPOSITORY_URI}:${tag}"
 }
