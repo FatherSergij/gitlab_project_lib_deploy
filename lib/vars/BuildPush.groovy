f2a7952136e4935780e1d0c85ab6b167dfe8363f
root user <roo@localhost>
2022-04-07 03:18:52 +0000

    h

--- lib/vars/BuildPush.groovy
+++ lib/vars/BuildPush.groovy
@@ -2,9 +2,12 @@
 import com.Constants
 
 def call(String branch, String tag, String service, String build_num) {
-    def REPOSITORY_URI="${Constants.AWS_ACCOUNT_ID}.dkr.ecr.${Constants.AWS_REGION}.amazonaws.com/${Constants.IMAGE_REPO_NAME}_${service}_${branch}"
+    def REPOSITORY_URI="${Constants.AWS_ACCOUNT_ID}.dkr.ecr.${Constants.AWS_REGION}.amazonaws.com/
+        ${Constants.IMAGE_REPO_NAME}_${service}_${branch}"
     def REPO_NAME="${Constants.IMAGE_REPO_NAME}_${service}_${branch}"
-    MANIFEST=sh(script: "aws ecr batch-get-image --repository-name ${REPO_NAME} --image-ids imageTag=latest --region ${Constants.AWS_REGION} --query 'images[].imageManifest' --output text")
+    MANIFEST=sh(script: "aws ecr batch-get-image --repository-name ${REPO_NAME} --image-ids imageTag=latest --region 
+        ${Constants.AWS_REGION} --query 'images[].imageManifest' --output text", returnStdout: true).trim()
+    echo "$MANIFEST"
     if ($MANIFEST != null) {
         sh(script: "aws ecr put-image --repository-name ${REPO_NAME} --image-tag ${build_num} --image-manifest ${MANIFEST} --region ${Constants.AWS_REGION}")
         sh(script: "aws ecr batch-delete-image --repository-name ${REPO_NAME} --image-ids imageTag=latest")
