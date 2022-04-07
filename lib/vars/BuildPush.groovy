3387b5f9535695ca1b1ce63bc2fcf776f81bb81f
root user <roo@localhost>
2022-04-07 13:30:44 +0000

    f

--- lib/vars/BuildPush.groovy
+++ lib/vars/BuildPush.groovy
@@ -7,7 +7,7 @@ def call(String branch, String tag, String service, String build_num) {
     MANIFEST_TMP=sh(script: "aws ecr batch-get-image --repository-name ${REPO_NAME} --image-ids imageTag=latest \
         --region ${Constants.AWS_REGION} --query 'images[].imageManifest' --output text")
     echo MANIFEST_TMP
-    if (MANIFEST_TMP == null) {
+    if (MANIFEST_TMP != null) {
         echo "not null"
     }
     // MANIFEST_TMP=sh(script: "aws ecr batch-get-image --repository-name ${REPO_NAME} --image-ids imageTag=latest \
