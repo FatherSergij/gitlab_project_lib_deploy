69a85595194f5a08d6c31d4fe5f2cb51ae6e3f37
root user <roo@localhost>
2022-04-07 08:57:31 +0000

    g

--- lib/vars/BuildPush.groovy
+++ lib/vars/BuildPush.groovy
@@ -6,10 +6,8 @@ def call(String branch, String tag, String service, String build_num) {
     def REPO_NAME="${Constants.IMAGE_REPO_NAME}_${service}_${branch}"
     MANIFEST_TMP=sh(script: "aws ecr batch-get-image --repository-name ${REPO_NAME} --image-ids imageTag=latest \
         --region ${Constants.AWS_REGION} --query 'images[0].imageManifest' --output json",returnStdout: true)
-   // if ( MANIFEST_TMP != null & MANIFEST_TMP != "null" & MANIFEST_TMP != "" ) {
-    if (MANIFEST_TMP) {
+    try {
         MANIFEST="${MANIFEST_TMP}".replace('\\n', '')
-//    if ("${MANIFEST}" != "null") {
         sh(script: "aws ecr put-image --repository-name ${REPO_NAME} --image-tag ${build_num} --region \
             ${Constants.AWS_REGION} --image-manifest ${MANIFEST}")
         sh(script: "aws ecr batch-delete-image --repository-name ${REPO_NAME} --region ${Constants.AWS_REGION} \
