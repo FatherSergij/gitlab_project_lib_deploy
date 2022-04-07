807240e5103e392f2b4683be6c8e19405f219772
root user <roo@localhost>
2022-04-07 05:49:44 +0000

    g

--- lib/vars/BuildPush.groovy
+++ lib/vars/BuildPush.groovy
@@ -4,8 +4,9 @@ import com.Constants
 def call(String branch, String tag, String service, String build_num) {
     def REPOSITORY_URI="${Constants.AWS_ACCOUNT_ID}.dkr.ecr.${Constants.AWS_REGION}.amazonaws.com/${Constants.IMAGE_REPO_NAME}_${service}_${branch}"
     def REPO_NAME="${Constants.IMAGE_REPO_NAME}_${service}_${branch}"
-    MANIFEST=sh(script: "aws ecr batch-get-image --repository-name ${REPO_NAME} --image-ids imageTag=latest --region \
-        ${Constants.AWS_REGION} --query 'images[].imageManifest' --output text")
+    MANIFEST_TMP=sh(script: "aws ecr batch-get-image --repository-name ${REPO_NAME} --image-ids imageTag=latest --region \
+        ${Constants.AWS_REGION} --query 'images[0].imageManifest' --output json",returnStdout: true)
+    MANIFEST="${MANIFEST_TMP}".replace('\\n', '')
     if ("${MANIFEST}" != "") {
         sh(script: "aws ecr put-image --repository-name ${REPO_NAME} --image-tag ${build_num} --region \
             ${Constants.AWS_REGION} --image-manifest ${MANIFEST}")
