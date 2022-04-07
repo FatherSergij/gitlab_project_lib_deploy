ff6898570248a882432aa48341d6f7e83da1d9db
root user <roo@localhost>
2022-04-07 02:43:26 +0000

    g

--- lib/vars/BuildPush.groovy
+++ lib/vars/BuildPush.groovy
@@ -6,7 +6,7 @@ def call(String branch, String tag, String service, String build_num) {
     def REPO_NAME="${Constants.IMAGE_REPO_NAME}_${service}_${branch}"
     MANIFEST=sh(script: "aws ecr batch-get-image --repository-name ${REPO_NAME} --image-ids imageTag=latest --output json | jq --raw-output --join-output '.images[0].imageManifest'")
     sh(script: "aws ecr put-image --repository-name ${REPO_NAME} --image-tag ${build_num} --image-manifest ${MANIFEST}")
-    sh(script: "aws ecr batch-delete-image --repository-name ${REPO_NAME} --image-ids imageTag=latest" 
+    sh(script: "aws ecr batch-delete-image --repository-name ${REPO_NAME} --image-ids imageTag=latest")
     sh "docker build src/ -t ${REPOSITORY_URI}:latest"
     sh "docker push ${REPOSITORY_URI}:latest"
 }
