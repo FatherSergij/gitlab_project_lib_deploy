dc7cf06193edfc440758636522fe606ff005c648
root user <roo@localhost>
2022-04-08 12:58:23 +0000

    f

--- lib/vars/BuildPush.groovy
+++ lib/vars/BuildPush.groovy
@@ -7,20 +7,12 @@ def call(String branch, String tag, String service, String build_num) {
     MANIFEST=sh(script: "aws ecr batch-get-image --repository-name ${REPO_NAME} --image-ids imageTag=latest \
         --region ${Constants.AWS_REGION} --query 'images[0].imageManifest' --output json",returnStdout: true)\
         .trim().replace('\\n', '')
-    //MANIFEST="${MANIFEST_TMP}".replace('\\n', '')
-    //try {    
-    //if ("${MANIFEST}" != "" || MANIFEST != null || "${MANIFEST}" != "null" || "${MANIFEST}" != '') {
-    // if ("${MANIFEST_TMP}".replace('\\n', '') == null) {
-    //     MANIFEST=null
-    // }
     if (MANIFEST != 'null') {
         sh(script: "aws ecr put-image --repository-name ${REPO_NAME} --image-tag ${build_num} --region \
             ${Constants.AWS_REGION} --image-manifest ${MANIFEST}")
         sh(script: "aws ecr batch-delete-image --repository-name ${REPO_NAME} --region ${Constants.AWS_REGION} \
             --image-ids imageTag=latest")
     }
-    //} finally {
     sh "docker build src/ -t ${REPOSITORY_URI}:${tag}"
     sh "docker push ${REPOSITORY_URI}:${tag}"
-    //}
 }
