c5f4bba180b316db281cee6b169774ff137687b7
root user <roo@localhost>
2022-04-08 11:48:45 +0000

    f

--- lib/vars/BuildPush.groovy
+++ lib/vars/BuildPush.groovy
@@ -4,21 +4,23 @@ import com.Constants
 def call(String branch, String tag, String service, String build_num) {
     def REPOSITORY_URI="${Constants.AWS_ACCOUNT_ID}.dkr.ecr.${Constants.AWS_REGION}.amazonaws.com/${Constants.IMAGE_REPO_NAME}_${service}_${branch}"
     def REPO_NAME="${Constants.IMAGE_REPO_NAME}_${service}_${branch}"
-    MANIFEST_TMP=sh(script: "aws ecr batch-get-image --repository-name ${REPO_NAME} --image-ids imageTag=latest \
-        --region ${Constants.AWS_REGION} --query 'images[0].imageManifest' --output json",returnStdout: true)
-    def MANIFEST="${MANIFEST_TMP}".replace('\\n', '')
-    try {    
+    MANIFEST=sh(script: "aws ecr batch-get-image --repository-name ${REPO_NAME} --image-ids imageTag=latest \
+        --region ${Constants.AWS_REGION} --query 'images[0].imageManifest' --output json",returnStdout: true)\
+        .trim().replace('\\n', '')
+    //MANIFEST="${MANIFEST_TMP}".replace('\\n', '')
+    //try {    
     //if ("${MANIFEST}" != "" || MANIFEST != null || "${MANIFEST}" != "null" || "${MANIFEST}" != '') {
     // if ("${MANIFEST_TMP}".replace('\\n', '') == null) {
     //     MANIFEST=null
     // }
-    //if (MANIFEST != 'null' && MANIFEST != null) {
+    if (MANIFEST != 'null') {
         sh(script: "aws ecr put-image --repository-name ${REPO_NAME} --image-tag ${build_num} --region \
             ${Constants.AWS_REGION} --image-manifest ${MANIFEST}")
         sh(script: "aws ecr batch-delete-image --repository-name ${REPO_NAME} --region ${Constants.AWS_REGION} \
             --image-ids imageTag=latest")
-    } finally {
+    }
+    //} finally {
     sh "docker build src/ -t ${REPOSITORY_URI}:${tag}"
     sh "docker push ${REPOSITORY_URI}:${tag}"
-    }
+    //}
 }
