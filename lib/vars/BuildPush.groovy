5bcabd45390fc7598197fa3d83dd3282f43b6c6f
root user <roo@localhost>
2022-04-08 07:57:00 +0000

    g

--- lib/vars/BuildPush.groovy
+++ lib/vars/BuildPush.groovy
@@ -4,37 +4,17 @@ import com.Constants
 def call(String branch, String tag, String service, String build_num) {
     def REPOSITORY_URI="${Constants.AWS_ACCOUNT_ID}.dkr.ecr.${Constants.AWS_REGION}.amazonaws.com/${Constants.IMAGE_REPO_NAME}_${service}_${branch}"
     def REPO_NAME="${Constants.IMAGE_REPO_NAME}_${service}_${branch}"
-    // MANIFEST_TMP=sh(script: "aws ecr batch-get-image --repository-name ${REPO_NAME} --image-ids imageTag=latest \
-    //     --region ${Constants.AWS_REGION} --query 'images[0].imageManifest' --output json")
-    // echo MANIFEST_TMP
-    // MANIFEST_TMP=sh("aws ecr list-images --repository-name ${REPO_NAME} --region ${Constants.AWS_REGION} \
-    //     --query 'imageIds[?imageTag==latest].imageTag' --output text")
-    // echo MANIFEST_TMP
-    // if (MANIFEST_TMP != null) {
-    //     echo "not null"
-    //     sh(script: "aws ecr put-image --repository-name ${REPO_NAME} --image-tag ${build_num} --region \
-    //         ${Constants.AWS_REGION} --image-manifest ${MANIFEST}")
-    //     sh(script: "aws ecr batch-delete-image --repository-name ${REPO_NAME} --region ${Constants.AWS_REGION} \
-    //         --image-ids imageTag=latest")        
-    // }
     MANIFEST_TMP=sh(script: "aws ecr batch-get-image --repository-name ${REPO_NAME} --image-ids imageTag=latest \
         --region ${Constants.AWS_REGION} --query 'images[0].imageManifest' --output json",returnStdout: true)
-    // def TR=echo MANIFEST_TMP
-    // echo TR
-    // echo "${TR}"
-    // if ("${TR}" != "null" & TR != null & "${TR}" != "")
-   // try {
-    echo MANIFEST_TMP
     def MANIFEST="${MANIFEST_TMP}".replace('\\n', '')
-    echo MANIFEST
-    if ("${MANIFEST}" != "" & MANIFEST != null & "${MANIFEST}" != "null" & "${MANIFEST}" != '') {
+    try {    
+    //if ("${MANIFEST}" != "" & MANIFEST != null & "${MANIFEST}" != "null" & "${MANIFEST}" != '') {
         sh(script: "aws ecr put-image --repository-name ${REPO_NAME} --image-tag ${build_num} --region \
             ${Constants.AWS_REGION} --image-manifest ${MANIFEST}")
         sh(script: "aws ecr batch-delete-image --repository-name ${REPO_NAME} --region ${Constants.AWS_REGION} \
             --image-ids imageTag=latest")
-    }
-    // } finally {
+    } finally {
     sh "docker build src/ -t ${REPOSITORY_URI}:${tag}"
     sh "docker push ${REPOSITORY_URI}:${tag}"
-    // }
+    }
 }
