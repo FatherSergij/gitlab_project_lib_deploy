eeb7faabccf2ef8816bd6c5bc25075b0bf5a32a4
root user <roo@localhost>
2022-04-14 12:52:36 +0000

    f

--- lib/vars/DeployHelm.groovy
+++ lib/vars/DeployHelm.groovy
@@ -1,6 +1,9 @@
 #!/usr/bin/env groovy
 import com.Constants
 
-def call(String branch_dep, String tag_dep, String service_dep) {
-
+def call(String branch_ng, String tag_ng, String branch_nd, String tag_nd) {
+    build job: 'Job_deploy_helm', parameters: [string(name: 'BranchRun_ng', value: "${branch_ng}"), 
+                  string(name: 'ImageTag_ng', value: "${tag_ng}"),
+                  string(name: 'BranchRun_nd', value: "${branch_nd}"),
+                  string(name: 'ImageTag_nd', value: "${tag_nd}")]
 }
