83bb611d9e5075acf18648dbf1ece27f7941e081
root user <roo@localhost>
2022-04-15 13:06:09 +0000

    f

--- lib/vars/DeployHelm.groovy
+++ lib/vars/DeployHelm.groovy
@@ -2,8 +2,8 @@
 import com.Constants
 
 def call(String branch_ng, String tag_ng, String branch_nd, String tag_nd) {
-    sh("scp -o StrictHostKeyChecking=no -r helm/ ubuntu@${IP_K8S}:~/")
-    sh('ssh ubuntu@${IP_K8S} """cd helm; \
+    sh("scp -o StrictHostKeyChecking=no -r helm/ ubuntu@${Constants.IP_K8S}:~/")
+    sh('ssh ubuntu@${Constants.IP_K8S} """cd helm; \
         sed -i.bak \'s/%BRNG%/${BRANCHNG}/; s/%TAGNG%/${TAGNG}/; \
             s/%BRND%/${BRANCHND}/; s/%TAGND%/${TAGND}/\' values.yaml; \
         helm upgrade --install test .;"""')
