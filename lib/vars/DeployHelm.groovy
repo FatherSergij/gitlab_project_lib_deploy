6972153557a896408100c6491a8048b0298f7e65
root user <roo@localhost>
2022-04-15 13:00:00 +0000

    f

--- lib/vars/DeployHelm.groovy
+++ lib/vars/DeployHelm.groovy
@@ -7,5 +7,4 @@ def call(String branch_ng, String tag_ng, String branch_nd, String tag_nd) {
         sed -i.bak \'s/%BRNG%/${BRANCHNG}/; s/%TAGNG%/${TAGNG}/; \
             s/%BRND%/${BRANCHND}/; s/%TAGND%/${TAGND}/\' values.yaml; \
         helm upgrade --install test .;"""')
-    } 
 }
