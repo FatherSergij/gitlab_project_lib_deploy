a323788b5a62636509aa8ff6efccbab856f1791c
root user <roo@localhost>
2022-04-16 07:02:42 +0000

    g

--- lib/vars/DeployHelm.groovy
+++ lib/vars/DeployHelm.groovy
@@ -7,7 +7,7 @@ def call(String branch_ng, String tag_ng, String branch_nd, String tag_nd) {
         sh("ssh ubuntu@${Constants.IP_K8S} 'cd helm; \
             sed -i.bak \"s/%BRNG%/${branch_ng}/; s/%TAGNG%/${tag_ng}/; \
                 s/%BRND%/${branch_nd}/; s/%TAGND%/${tag_nd}/\" values.yaml; \
-            export KUBECONFIG=/home/ubuntu/helm/kubernetes-deploy-helm-kube.conf \
+            export KUBECONFIG=\"/home/ubuntu/helm/kubernetes-deploy-helm-kube.conf\" \
             echo $KUBECONFIG \
             helm upgrade --install --create-namespace -n my-project --kubeconfig \
                 kubernetes-deploy-helm-kube.conf --kube-as-user deploy-helm test .;'")
