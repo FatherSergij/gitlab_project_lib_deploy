0d7ce858712a5b3730144e05a6f57385087d1665
root user <roo@localhost>
2022-04-17 13:54:51 +0000

    d

--- lib/vars/DeployHelm.groovy
+++ lib/vars/DeployHelm.groovy
@@ -5,8 +5,9 @@ def call(String branch_ng, String tag_ng, String branch_nd, String tag_nd) {
     sh("scp -o StrictHostKeyChecking=no -r helm/ ubuntu@${Constants.IP_K8S}:~/")
     //withCredentials([file(credentialsId: 'deployhelm', variable: 'deployhelm')]) {
         sh("ssh ubuntu@${Constants.IP_K8S} 'cd helm; \
-            env; \
             sed -i.bak \"s/%BRNG%/${branch_ng}/; s/%TAGNG%/${tag_ng}/; \
-                s/%BRND%/${branch_nd}/; s/%TAGND%/${tag_nd}/\" values.yaml;'")
+                s/%BRND%/${branch_nd}/; s/%TAGND%/${tag_nd}/\" values.yaml; \
+            helm upgrade --install -n my-project --kubeconfig \
+                kubernetes-deploy-helm-kube.conf test .;'")
    // }
 }
