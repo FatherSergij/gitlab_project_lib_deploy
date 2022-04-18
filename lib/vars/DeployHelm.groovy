5c723854d575a4fb3c54f63bbac03c14cd896ad0
root user <roo@localhost>
2022-04-18 13:23:35 +0000

    g

--- lib/vars/DeployHelm.groovy
+++ lib/vars/DeployHelm.groovy
@@ -3,7 +3,6 @@ import com.Constants
 
 def call(String branch_ng, String tag_ng, String branch_nd, String tag_nd) {
     //sh("scp -o StrictHostKeyChecking=no -r helm/ ubuntu@${Constants.IP_K8S}:~/")
-    //sh("git clone git@github.com:FatherSergij/project_helmchart.git/helm/ ubuntu@${Constants.IP_K8S}:~/")
     //withCredentials([file(credentialsId: 'deployhelm', variable: 'deployhelm')]) {
         sh("ssh ubuntu@${Constants.IP_K8S} 'git clone git@github.com:FatherSergij/project_helmchart.git helm; \
             cd helm; \
