59a02c86fcb9f1c766fea7a3d0ef439b522da26a
root user <roo@localhost>
2022-04-18 13:34:12 +0000

    f

--- lib/vars/DeployHelm.groovy
+++ lib/vars/DeployHelm.groovy
@@ -2,10 +2,9 @@
 import com.Constants
 
 def call(String branch_ng, String tag_ng, String branch_nd, String tag_nd) {
-    //sh("scp -o StrictHostKeyChecking=no -r helm/ ubuntu@${Constants.IP_K8S}:~/")
+    sh("scp -o StrictHostKeyChecking=no -r helm/ ubuntu@${Constants.IP_K8S}:~/")
     //withCredentials([file(credentialsId: 'deployhelm', variable: 'deployhelm')]) {
-        sh("ssh ubuntu@${Constants.IP_K8S} 'git clone git@github.com:FatherSergij/project_helmchart.git helm; \
-            cd helm; \
+        sh("ssh ubuntu@${Constants.IP_K8S} 'cd helm; \
             sed -i.bak \"s/%BRNG%/${branch_ng}/; s/%TAGNG%/${tag_ng}/; \
                 s/%BRND%/${branch_nd}/; s/%TAGND%/${tag_nd}/\" values.yaml; \
             helm upgrade --install -n my-project --kubeconfig \
