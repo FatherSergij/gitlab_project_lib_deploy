ae11d7da045fceb4fd28b9b43ad791767e407ef8
ubuntu user <ubuntu@localhost>
2022-04-24 13:40:46 +0000

    33

--- lib/src/com/Constants.groovy
+++
@@ -1,8 +0,0 @@
-package com
-
-class Constants {
-    static final String IP_K8S="16.170.42.2"
-    static final String AWS_ACCOUNT_ID="728490037630"
-    static final String AWS_REGION="eu-north-1" 
-    static final String IMAGE_REPO_NAME="bigproject"
-}
--- lib/vars/LogToEcr.groovy
+++
@@ -1,6 +0,0 @@
-import com.Constants
-
-def call() {
-    sh("aws ecr get-login-password --region ${Constants.AWS_REGION} | docker login --username AWS \
-      --password-stdin ${Constants.AWS_ACCOUNT_ID}.dkr.ecr.${Constants.AWS_REGION}.amazonaws.com")
-}
